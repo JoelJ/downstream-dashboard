9b524c54f2e282e2729db507455d1e9e9ff2a8f4
joeljohnson <joeljohnson@attask.com>
2013-03-25 23:23:08 -0600

    apparently it's possible for there to be a null action

--- src/main/webapp/scripts/downstreamDashboard.js
+++ src/main/webapp/scripts/downstreamDashboard.js
@@ -33,17 +33,19 @@ function TableControl($scope, $http, $location) {
 	$scope.normalizeActions = function(run) {
 		for (var actionIndex = run.actions.length - 1; actionIndex >= 0; actionIndex--) {
 			var action = run.actions[actionIndex];
-			if (action.downstreamBuilds) {
-				run.downstreamBuilds = action.downstreamBuilds;
-				for (var downstreamIndex = run.downstreamBuilds.length - 1; downstreamIndex >= 0; downstreamIndex--) {
-					var downstreamBuild = run.downstreamBuilds[downstreamIndex];
-					$scope.normalizeActions(downstreamBuild);
+			if(action) {
+				if (action.downstreamBuilds) {
+					run.downstreamBuilds = action.downstreamBuilds;
+					for (var downstreamIndex = run.downstreamBuilds.length - 1; downstreamIndex >= 0; downstreamIndex--) {
+						var downstreamBuild = run.downstreamBuilds[downstreamIndex];
+						$scope.normalizeActions(downstreamBuild);
+					}
+				} else if (action.failCount !== undefined) {
+					run.failCount = action.failCount;
+					run.totalCount = action.totalCount;
+					run.skipCount = action.skipCount;
+					run.urlName = action.urlName;
 				}
-			} else if (action.failCount !== undefined) {
-				run.failCount = action.failCount;
-				run.totalCount = action.totalCount;
-				run.skipCount = action.skipCount;
-				run.urlName = action.urlName;
 			}
 		}
 	};
